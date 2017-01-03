b5ecc08e93cce687a30dfd7eb435bfb631727e5b
Luis Hdez <luis.munoz.hdez@gmail.com>
2017-01-03 02:39:54 +0100

    Update docker-compose for vagrant

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -2,5 +2,5 @@
 
 # setup docker-compose as sudo
 mkdir -p /opt/bin
-curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /opt/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /opt/bin/docker-compose
 chmod +x /opt/bin/docker-compose
